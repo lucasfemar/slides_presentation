c9a1da9c2edad522834e5b42497bc481f09f2789
Lucas Marcelo <65972119+lucasfemar@users.noreply.github.com>
2024-12-05 23:04:12 +0000

    add tela setup

---
+++ src/pages/setup/styles.ts
@@ -0,0 +1,21 @@
+// import styled from 'styled-components';
+
+// export const Content = styled.div`
+//     position: relative;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     /* height: calc(100vh - var(--header-height) - var(--footer-height)); // Altura entre header e footer */
+//     /* width: 100%; */
+//     /* padding: 1rem; */
+//     /* box-sizing: border-box; */
+
+//     @media (max-width: 768px) {
+//         height: calc(100vh - var(--header-height) - var(--footer-height) - 20px);
+//         padding: 0.5rem; // Ajusta o padding em telas menores
+//     }
+
+//     @media (min-width: 1920px) {
+//         padding: 2rem; // Ajuste para telas maiores
+//     }
+// `;
