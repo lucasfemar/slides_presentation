3d1fad06a3162d1c6bd01d69376ffc9d9f1ee090
Lucas Marcelo <lucasdeveloper98l@gmail.com>
2025-02-19 22:01:55 -0300

    refactor: use `next-connect` to handler `api/v1/use`

--- src/pages/api/v1/user/index.ts
+++ src/pages/api/v1/user/index.ts
@@ -2,82 +2,67 @@ import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "lib/prisma";
 import { z } from "zod";
 import bcrypt from "bcrypt";
+import { createRouter } from "next-connect";
 
-interface IApiService {
-  GET: (request?: NextApiRequest) => Promise<{ result: any; status: number }>;
-  POST: (request: NextApiRequest) => Promise<{ result: any; status: number }>;
-}
+const router = createRouter<NextApiRequest, NextApiResponse>();
 
-const apiService: IApiService = {
-  GET: async () => {
-    try {
-      const users = await prisma.users.findMany();
-      return { result: users, status: 200 };
-    } catch (error) {
-      if (error instanceof Error) {
-        throw new Error(`Falha ao buscar usuários: ${error.message}`);
-      } else {
-        throw new Error(
-          "Ocorreu um erro desconhecido ao buscar usuáriosn unknown error occurred while fetching users",
-        );
-      }
-    }
+router.get(getHandler);
+router.post(postHandler);
+
+export default router.handler({
+  onNoMatch: (request: NextApiRequest, response: NextApiResponse) => {
+    return response.status(401).json({
+      message: "Erro on Match",
+    });
   },
-
-  POST: async (request) => {
-    try {
-      const userSchema = z.object({
-        name: z.string(),
-        phone: z.string(),
-        ministery: z.string(),
-        email: z.string().email(),
-        password: z.string(),
-        status: z.boolean(),
-      });
-
-      const user = userSchema.parse(request.body);
-
-      const emailExists = await prisma.users.findUnique({
-        where: { email: user.email },
-      });
-
-      if (emailExists) {
-        throw new Error("Este email já existe");
-      }
-      user.password = bcrypt.hashSync(user.password, 10);
-      const createdUser = await prisma.users.create({ data: user });
-      return { result: createdUser, status: 201 };
-    } catch (error) {
-      throw new Error(
-        `Falha ao criar usuário: ${error instanceof Error ? error.message : "Unknown error"}`,
-      );
-    }
+  onError: (error, request: NextApiRequest, response: NextApiResponse) => {
+    return response.status(401).json({
+      message: "Error on error",
+    });
   },
-};
+});
 
-async function handleRequest(
-  request: NextApiRequest,
-  response: NextApiResponse,
-) {
-  const ALLOWED_METHODS: Array<keyof IApiService> = ["GET", "POST"];
-  const method = request.method as keyof IApiService;
-
-  if (!ALLOWED_METHODS.includes(method)) {
-    return response.status(405).json({ message: "Método não permitido" });
-  }
-
+async function getHandler(request: NextApiRequest, response: NextApiResponse) {
   try {
-    const { result, status } = await apiService[method](request);
-    return response.status(status).json(result);
+    const users = await prisma.users.findMany();
+    return response.status(200).json(users);
   } catch (error) {
     if (error instanceof Error) {
-      return response.status(500).json({ error: error.message });
+      throw new Error(`Falha ao buscar usuários: ${error.message}`);
     } else {
-      return response
-        .status(500)
-        .json({ error: "Ocorreu um erro desconhecido" });
+      throw new Error(
+        "Ocorreu um erro desconhecido ao buscar usuáriosn unknown error occurred while fetching users",
+      );
     }
   }
 }
 
-export default handleRequest;
+async function postHandler(request: NextApiRequest, response: NextApiResponse) {
+  try {
+    const userSchema = z.object({
+      name: z.string(),
+      phone: z.string(),
+      ministery: z.string(),
+      email: z.string().email(),
+      password: z.string(),
+      status: z.boolean(),
+    });
+
+    const user = userSchema.parse(request.body);
+
+    const emailExists = await prisma.users.findUnique({
+      where: { email: user.email },
+    });
+
+    if (emailExists) {
+      throw new Error("Este email já existe");
+    }
+    user.password = bcrypt.hashSync(user.password, 10);
+    const createdUser = await prisma.users.create({ data: user });
+    return response.status(201).json(createdUser);
+  } catch (error) {
+    throw new Error(
+      `Falha ao criar usuário: ${error instanceof Error ? error.message : "Unknown error"}`,
+    );
+  }
+}
