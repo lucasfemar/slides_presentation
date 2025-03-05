9fb38c714bd3cd8d04cda00b5b400ff7d7861e68
Lucas Marcelo <lucasdeveloper98l@gmail.com>
2025-03-05 20:24:38 -0300

    add `globastyle.tsx` and `themestyle.tsx` and use it

---
+++ src/components/themestyle.tsx
@@ -0,0 +1,15 @@
+export const Theme = {
+    colors: {
+        background: '#f7f7f7',
+        cardBackground: 'white',
+        borderColor: '#ccc',
+        primary: '#8a3489',
+        primaryHover: '#0056b3',
+        focusColor: '#007bff',
+        titleColor: '#0f62ab',
+        linkColor: '#007bff',
+        textColor: 'black',
+        footerBackground: '#ffffff',
+        footerShadow: 'rgba(0, 0, 0, 0.1)',
+    },
+};
